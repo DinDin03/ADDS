ab38de2c34596aee1c7b8ef89cbb3a93833e4451
DinDin03 <dineth.himasha@outlook.com>
2023-08-27 22:22:01 +0930

    adding files

---
+++ Finder.cpp
@@ -0,0 +1,26 @@
+#include "Finder.h"
+
+std::vector<int> Finder::findSubstrings(const std::string& s1, const std::string& s2) {
+    std::vector<int> result;
+    int n = s1.size();
+    int m = s2.size();
+    
+    for (int i = 0; i <= n - m; i++) {
+        bool match = true;
+        for (int j = 0; j < m; j++) {
+            if (s1[i + j] != s2[j]) {
+                match = false;
+                break;
+            }
+        }
+        if (match) {
+            result.push_back(i);
+        }
+    }
+    
+    if (result.empty()) {
+        result.push_back(-1);
+    }
+    
+    return result;
+}
---
+++ Finder.h
@@ -0,0 +1,12 @@
+#ifndef FINDER_H
+#define FINDER_H
+
+#include <vector>
+#include <string>
+
+class Finder {
+public:
+    static std::vector<int> findSubstrings(const std::string& s1, const std::string& s2);
+};
+
+#endif // FINDER_H
