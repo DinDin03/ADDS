7186fc4580c25dd2482cf5df6e54f5e48db2ecec
DinDin03 <dineth.himasha@outlook.com>
2023-09-04 18:19:34 +0930

    adding files

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include "Sort.h"
 
 int main() {
-  // Read input list from stdin
   std::vector<int> inputList;
   int num;
 
@@ -15,24 +14,18 @@ int main() {
     inputList.push_back(num);
   }
 
-  // Create instances of sorting and searching classes
   Sort* sorter = new QuickSort();
   RecursiveBinarySearch searcher;
 
-  // Sort the input list
   std::vector<int> sortedList = sorter->sort(inputList);
-
-  // Search for 1 in the sorted list
   bool found = searcher.search(sortedList, 1);
 
-  // Print the result
   std::cout << (found ? "true" : "false");
   for (int num : sortedList) {
     std::cout << " " << num;
   }
   std::cout << std::endl;
 
-  // Clean up
   delete sorter;
 
   return 0;
