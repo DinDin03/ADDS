270780ac30963252f09c99e1768a0658a03c2d42
DinDin03 <dineth.himasha@outlook.com>
2023-09-11 21:27:49 +0930

    adding files

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <algorithm>
 
-std::list<int> BigNumCalc::buildBigNum(const std::string& numString) {
+std::list<int> bigNumCalc::buildBigNum(const std::string& numString) {
     std::list<int> result;
     for (char c : numString) {
         if (std::isdigit(c)) {
@@ -13,7 +13,7 @@ std::list<int> BigNumCalc::buildBigNum(const std::string& numString) {
     return result;
 }
 
-std::list<int> BigNumCalc::add(const std::list<int>& num1, const std::list<int>& num2) {
+std::list<int> bigNumCalc::add(const std::list<int>& num1, const std::list<int>& num2) {
     std::list<int> result;
     int carry = 0;
 
@@ -41,7 +41,7 @@ std::list<int> BigNumCalc::add(const std::list<int>& num1, const std::list<int>&
     return result;
 }
 
-std::list<int> BigNumCalc::sub(const std::list<int>& num1, const std::list<int>& num2) {
+std::list<int> bigNumCalc::sub(const std::list<int>& num1, const std::list<int>& num2) {
     std::list<int> result;
     int borrow = 0;
 
@@ -77,7 +77,7 @@ std::list<int> BigNumCalc::sub(const std::list<int>& num1, const std::list<int>&
     return result;
 }
 
-std::list<int> BigNumCalc::mul(const std::list<int>& num1, int num2) {
+std::list<int> bigNumCalc::mul(const std::list<int>& num1, int num2) {
     std::list<int> result;
     int carry = 0;
 
@@ -91,6 +91,38 @@ std::list<int> BigNumCalc::mul(const std::list<int>& num1, int num2) {
     if (carry) {
         result.push_front(carry);
     }
+
+    return result;
+}
+
+// Implement multiplication of two big numbers represented as lists
+std::list<int> bigNumCalc::mul(const std::list<int>& num1, const std::list<int>& num2) {
+    std::list<int> result(num1.size() + num2.size(), 0);
+
+    auto it1 = num1.rbegin();
+    int carry = 0;
+
+    for (int i = 0; it1 != num1.rend(); ++it1, ++i) {
+        auto it2 = num2.rbegin();
+        int k = i;
+        carry = 0;
+
+        for (; it2 != num2.rend(); ++it2, ++k) {
+            int product = (*it1 * *it2) + result[k] + carry;
+            carry = product / 10;
+            result[k] = product % 10;
+        }
+
+        while (carry) {
+            int sum = result[k] + carry;
+            carry = sum / 10;
+            result[k] = sum % 10;
+            ++k;
+        }
+    }
+
+    // Remove leading zeros
+    result.erase(result.begin(), std::find_if(result.begin(), result.end(), [](int x) { return x != 0; }));
 
     return result;
 }
