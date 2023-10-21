22f4c310941fd2c21c9f985835367fce2ae6ed69
DinDin03 <dineth.himasha@outlook.com>
2023-10-21 22:45:16 +1030

    adding files

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -1,62 +1,55 @@
 #include "DocumentManager.h"
-using namespace std;
 
-struct Document {
-    string name;
-    int id;
-    int license_limit;
-    int borrowed_count;
-    unordered_set<int> patrons_borrowed;
-};
+// Constructor (if any specific initialization is required)
+DocumentManager::DocumentManager() {
+    // Initialization code (if any)
+}
 
-class DocumentManager {
-private:
-    unordered_map<string, int> name_to_docid;
-    unordered_map<int, Document> documents;
-    set<int> registered_patrons;
+// Implement the addDocument method
+void DocumentManager::addDocument(const std::string& name, int id, int license_limit) {
+    Document doc = {name, id, license_limit, 0};
+    name_to_docid[name] = id;
+    documents[id] = doc;
+}
 
-public:
-    void addDocument(string name, int id, int license_limit) {
-        Document doc = {name, id, license_limit, 0};
-        name_to_docid[name] = id;
-        documents[id] = doc;
+// Implement the addPatron method
+void DocumentManager::addPatron(int patronID) {
+    registered_patrons.insert(patronID);
+}
+
+// Implement the search method
+int DocumentManager::search(const std::string& name) {
+    if (name_to_docid.find(name) != name_to_docid.end()) {
+        return name_to_docid[name];
+    }
+    return 0; // not found
+}
+
+// Implement the borrowDocument method
+bool DocumentManager::borrowDocument(int docid, int patronID) {
+    if (documents.find(docid) == documents.end() || 
+        registered_patrons.find(patronID) == registered_patrons.end()) {
+        return false; // Invalid docid or patronID
     }
 
-    void addPatron(int patronID) {
-        registered_patrons.insert(patronID);
+    Document &doc = documents[docid];
+    if (doc.borrowed_count < doc.license_limit &&
+        doc.patrons_borrowed.find(patronID) == doc.patrons_borrowed.end()) {
+        doc.borrowed_count++;
+        doc.patrons_borrowed.insert(patronID);
+        return true;
     }
+    return false; // Can't be borrowed
+}
 
-    int search(string name) {
-        if (name_to_docid.find(name) != name_to_docid.end()) {
-            return name_to_docid[name];
-        }
-        return 0; // not found
-    }
-
-    bool borrowDocument(int docid, int patronID) {
-        if (documents.find(docid) == documents.end() || 
-            registered_patrons.find(patronID) == registered_patrons.end()) {
-            return false; // Invalid docid or patronID
-        }
-
+// Implement the returnDocument method
+void DocumentManager::returnDocument(int docid, int patronID) {
+    if (documents.find(docid) != documents.end() &&
+        registered_patrons.find(patronID) != registered_patrons.end() &&
+        documents[docid].patrons_borrowed.find(patronID) != documents[docid].patrons_borrowed.end()) {
+        
         Document &doc = documents[docid];
-        if (doc.borrowed_count < doc.license_limit &&
-            doc.patrons_borrowed.find(patronID) == doc.patrons_borrowed.end()) {
-            doc.borrowed_count++;
-            doc.patrons_borrowed.insert(patronID);
-            return true;
-        }
-        return false; // Can't be borrowed
+        doc.borrowed_count--;
+        doc.patrons_borrowed.erase(patronID);
     }
-
-    void returnDocument(int docid, int patronID) {
-        if (documents.find(docid) != documents.end() &&
-            registered_patrons.find(patronID) != registered_patrons.end() &&
-            documents[docid].patrons_borrowed.find(patronID) != documents[docid].patrons_borrowed.end()) {
-            
-            Document &doc = documents[docid];
-            doc.borrowed_count--;
-            doc.patrons_borrowed.erase(patronID);
-        }
-    }
-};
+}
